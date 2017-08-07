a354027e5b5c174038f956f4aab4a71840d09d57
timeTravelCYN <554793916@qq.com>
2017-08-07 11:06:01 +0800

    home骨架开发

--- vue-erp/src/router/index.js
+++ vue-erp/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
 
 Vue.use(Router)
 
@@ -8,6 +7,10 @@ export default new Router({
   routes: [{
       path: '/',
       redirect: '/login'
+    },
+    {
+      path: '/readme',
+      component: resolve => require(['../components/common/Home.vue'], resolve)
     },
     {
       path: '/login',

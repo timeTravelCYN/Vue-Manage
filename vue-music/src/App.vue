b04002284e7f28d8c9a8845921c7e9cc33be2b9d
timeTravelCYN <554793916@qq.com>
2017-08-13 23:00:13 +0800

    vue-music项目

---
+++ vue-music/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+    <tab></tab>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import MHeader from 'components/m-header/m-header'
+import Tab from 'components/tab/tab'
+export default {
+  name: 'app',
+  components: {
+    MHeader,
+    Tab
+  }
+}
+</script>
+
+<style lang="scss">
+/* 
+  ~ 是sass-loader的一种处理，相当于映射到相对的common目录，webpackk里面的alias。 sass-loader官网有处理
+*/
+
+@import '~common/scss/variable';
+#app {
+  color: $color-theme;
+}
+</style>
+
---
+++ vue-music/src/components/rank/rank.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="rank">
+    这是rank
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
+

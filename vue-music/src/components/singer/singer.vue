60808f7d50bffb2223ceab50322e3fe11ae3eb1a
timeTravelCYN <554793916@qq.com>
2017-08-18 18:07:56 +0800

    歌手列表

--- vue-music/src/components/singer/singer.vue
+++ vue-music/src/components/singer/singer.vue
@@ -5,13 +5,37 @@
 </template>
 
 <script>
+import { getSingerList } from 'api/singer'
+import { ERR_OK } from 'api/config'
 export default {
-  
+  data() {
+    return {
+      singers: []
+    }
+  },
+  created() {
+    this._getSingerList()
+  },
+  methods: {
+    _getSingerList() {
+      getSingerList().then((res) => {
+        if(res.code === ERR_OK) {
+          this.singers = res.data.list
+          console.log(this.singers)
+        }
+      })
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-
+.singer {
+  position: fixed;
+  top: 88px;
+  bottom: 0;
+  width: 100%;
+}
 </style>
 
 
